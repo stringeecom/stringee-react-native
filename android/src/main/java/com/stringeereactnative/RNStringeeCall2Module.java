91866e800ad142294fc950ffc271e1e4ac2c5410
AnhNQK <nguyenquangkyanh@gmail.com>
2021-02-04 17:17:39 +0700

    - fix bug switch camera - upgrade version

--- android/src/main/java/com/stringeereactnative/RNStringeeCall2Module.java
+++ android/src/main/java/com/stringeereactnative/RNStringeeCall2Module.java
@@ -365,8 +365,12 @@ public class RNStringeeCall2Module extends ReactContextBaseJavaModule implements
             callback.invoke(false, -3, "The call is not found.");
             return;
         }
-        call.switchCamera(null);
-        callback.invoke(true, 0, "Success");
+        call.switchCamera(new StatusListener() {
+            @Override
+            public void onSuccess() {
+                callback.invoke(true, 0, "Success");
+            }
+        });
     }
 
     @ReactMethod
--- android/src/main/java/com/stringeereactnative/RNStringeeCallModule.java
+++ android/src/main/java/com/stringeereactnative/RNStringeeCallModule.java
@@ -374,8 +374,12 @@ public class RNStringeeCallModule extends ReactContextBaseJavaModule implements 
             callback.invoke(false, -3, "The call is not found.");
             return;
         }
-        call.switchCamera(null);
-        callback.invoke(true, 0, "Success");
+        call.switchCamera(new StatusListener() {
+            @Override
+            public void onSuccess() {
+                callback.invoke(true, 0, "Success");
+            }
+        });
     }
 
     @ReactMethod
